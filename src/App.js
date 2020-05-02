13034eead4c473002e1e5a93868181294be55ace
Jack Hu <jackhu242@gmail.com>
2020-05-02 13:14:35 -0600

    Setup the patient record viewing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import { ReactComponent as Logo } from './assets/medical.svg'
+import { Puff } from 'svg-loaders-react';
+import { PatientRecord } from './components';
 
 // Colors: Ivory White: FFFFF0, English Vermillion D64952, Neutral White: FFFFFF
 
@@ -10,7 +12,8 @@ export default class App extends Component {
     super(props);
     this.state = {
       searched: false,
-      resultsPending: false
+      resultsPending: false,
+      data: {}
     }
   }
 
@@ -18,18 +21,32 @@ export default class App extends Component {
     this.setState({
       resultsPending: true
     });
+
+    setTimeout(() => {
+      this.setState({
+        resultsPending: false,
+        searched: true
+      });
+    }, 3000);
   }
 
   render() {
+
+    let { searched, resultsPending, data } = this.state;
+
     return (
       <div className='AppContainer'>
         <Logo className='Logo' />
         <div className='DocumentContainer'>
-          <input className='MainSearch' placeholder='Search for a patient...' onKeyPress={event => {
-            if (event.key === 'enter') {
+          <input className='MainSearch' placeholder='Search for a patient (ID No. or name)...' onKeyPress={event => {
+            if (event.key === 'Enter') {
               this.search();
             }
           }} />
+          <div className='Conditional'>
+            {resultsPending ? <Puff className='Spinner' stroke='#D64952' /> : null}
+            {searched ? <PatientRecord data={data} /> : null}
+          </div>
         </div>
       </div>
     );
