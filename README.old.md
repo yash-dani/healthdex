8598e5f07b01f38e98931c02ebcbe11257ec2e8b
Jack Hu <jackhu242@gmail.com>
2020-05-02 12:27:44 -0600

    Removed unneeded skeleton, added basic UI

--- README.old.md
+++
@@ -1,3 +0,0 @@
-# TOHacks 2020 
-
-This is our project for TOHacks 2020. It involved making it easier for healthcare professionals to interact with documents
--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { ReactComponent as Logo } from './assets/medical.svg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Colors: Ivory White: FFFFF0, English Vermillion D64952, Neutral White: FFFFFF
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searched: false,
+      resultsPending: false
+    }
+  }
+
+  async search() {
+    this.setState({
+      resultsPending: true
+    });
+  }
+
+  render() {
+    return (
+      <div className='AppContainer'>
+        <Logo className='Logo' />
+        <div className='DocumentContainer'>
+          <input className='MainSearch' placeholder='Search for a patient...' onKeyPress={event => {
+            if (event.key === 'enter') {
+              this.search();
+            }
+          }} />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
